f0bf718685907d73344ac7edf32927c76ed220d7
Nelson GT <nelsonjesusGT@gmail.com>
2026-06-22 10:05:05 -0400

    Hot reload backend

--- cofersa-api/main.py
+++ cofersa-api/main.py
@@ -717,4 +717,4 @@ async def import_presupuesto(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
